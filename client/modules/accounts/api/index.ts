38d1119f36d0c51607b22a83c3f8c190ebba4ecc
Andrii Miliuchenko <andrii2003@gmail.com>
2024-07-28 18:32:39 +0300

    refactor

--- client/modules/accounts/api/index.ts
+++ client/modules/accounts/api/index.ts
@@ -1,21 +1,23 @@
 import { api } from "@/lib/api";
 import { ApiListResponse } from "@/types/api";
-import { Account, GetAccountsListQueryParams } from "../types";
+import {
+  Account,
+  CreateAccountBody,
+  GetAccountsListQueryParams,
+  UpdateAccountBody,
+} from "../types";
 import { apiRoutes } from "@/constants";
 
 export const getAccountsListApi = (searchParams: GetAccountsListQueryParams) =>
   api.get(apiRoutes.account, { searchParams }).json<ApiListResponse<Account>>();
 
-export const createAccountApi = (json: Omit<Account, "id" | "createdAt">) =>
+export const createAccountApi = (json: CreateAccountBody) =>
   api.post(apiRoutes.account, { json }).json<Account>();
 
 export const getAccountById = (id: number) =>
   api.get(`${apiRoutes.account}/${id}`).json<Account>();
 
-export const updateAccountApi = ({
-  id,
-  name,
-}: Omit<Account, "userId" | "createdAt">) =>
+export const updateAccountApi = ({ id, name }: UpdateAccountBody) =>
   api.put(`${apiRoutes.account}/${id}`, { json: { name } }).json<Account>();
 
 export const deleteAccountsApi = (ids: number[]) =>
--- client/modules/categories/api/index.ts
+++ client/modules/categories/api/index.ts
@@ -1,6 +1,11 @@
 import { api } from "@/lib/api";
 import { ApiListResponse } from "@/types/api";
-import { Category, GetCategoriesListQueryParams } from "../types";
+import {
+  Category,
+  CreateCategoryBody,
+  GetCategoriesListQueryParams,
+  UpdateCategoryBody,
+} from "../types";
 import { apiRoutes } from "@/constants";
 
 export const getCategoriesListApi = (
@@ -10,16 +15,13 @@ export const getCategoriesListApi = (
     .get(apiRoutes.category, { searchParams })
     .json<ApiListResponse<Category>>();
 
-export const createCategoryApi = (json: Omit<Category, "id" | "createdAt">) =>
+export const createCategoryApi = (json: CreateCategoryBody) =>
   api.post(apiRoutes.category, { json }).json<Category>();
 
 export const getCategoryById = (id: number) =>
   api.get(`${apiRoutes.category}/${id}`).json<Category>();
 
-export const updateCategoryApi = ({
-  id,
-  name,
-}: Omit<Category, "userId" | "createdAt">) =>
+export const updateCategoryApi = ({ id, name }: UpdateCategoryBody) =>
   api.put(`${apiRoutes.category}/${id}`, { json: { name } }).json<Category>();
 
 export const deleteCategoriesApi = (ids: number[]) =>
